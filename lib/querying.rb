27249e3e72f297a4d2866c489e8a3acd219a5dd8
Catherine Salazar <catcreates13@gmail.com>
2018-08-19 17:03:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,8 +10,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters
-  GROUP BY species"
+  "SELECT characters.species, COUNT(characters.species) 
+  FROM characters
+  GROUP BY species
+  COUNT(characters.species) > 0"
 end
 
 def select_name_and_series_subgenres_of_authors
