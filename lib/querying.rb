2ff5fc0c457065e6276c008b7f37916b7fb34c58
Catherine Salazar <catcreates13@gmail.com>
2018-08-19 17:21:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,6 +26,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   INNER JOIN characters
   ON characters.series_id = series.id
+  GROUP BY series
   WHERE characters.species = 'human'"
 end
 
