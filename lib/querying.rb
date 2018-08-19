f67e741c120ed235d3c6c1a96966ba9405f87dd9
Catherine Salazar <catcreates13@gmail.com>
2018-08-19 17:09:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT name, series.subgenre_id FROM authors
-  INNER JOIN SERIES"
+  INNER JOIN series"
 end
 
 def select_series_title_with_most_human_characters
