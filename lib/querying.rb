fc169b6328dc4e591615e6a2b14f7f4845a40ddd
Catherine Salazar <catcreates13@gmail.com>
2018-08-19 17:03:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) 
+  "SELECT characters.species, COUNT(characters.species)
   FROM characters
   GROUP BY species
   COUNT(characters.species) > 0"
